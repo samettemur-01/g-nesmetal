9b5e94abd3d150ab8e7c945bc179aa92f0beb4cd
samettemur-01 <116579080+samettemur-01@users.noreply.github.com>
2023-01-18 22:30:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
+
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');  
+}
+
+
+
+
+
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () =>{
+    header.classList.toggle('shadow' , window.scrollY > 0)
+})
+
+
+
+    
